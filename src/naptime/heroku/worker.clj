36bfdc3a0f58f365c8fc4b07d1e919070aa3cbb6
zkim <zachary.kim@gmail.com>
2011-09-17 17:38:32 -0600

    Added worker id to support better logging. Cleaned up worker source.

--- src/naptime/heroku/worker.clj
+++ src/naptime/heroku/worker.clj
@@ -7,20 +7,28 @@
 (mon/mongo! :db :naptime)
 (mon/add-index! :jobs [:next-update])
 
+(try
+  (mon/create-collection! :job-logs :capped true :size (* 1024 1024 10))
+  (catch Exception e
+    (println "job-logs collection already exists.")))
+
+(try
+  (mon/create-collection! :worker-logs :capped true :size (* 1024 1024 10))
+  (catch Exception e
+    (println "worker-logs collection already exists.")))
+
+
 (def ^{:doc "Max request threads"} *max-capacity* 20)
 (def ^{:doc "Sleep time per run loop iteration"} *sleep-time* 10)
 
 (def *used-capacity* (atom 0))
 
-(defn run! []
+(defn run! [worker-id]
   (while true
-    (w/run-loop! *used-capacity* *max-capacity*)
+    (w/run-loop! worker-id *used-capacity* *max-capacity*)
     (Thread/sleep *sleep-time*)))
 
-(run!)
-
-(println @*used-capacity*)
-
+(run! (str (java.util.UUID/randomUUID)))
 
 #_(doseq [n (range 10)]
   (mon/insert! :jobs {:endpoint "http://google.com"
--- src/naptime/worker.clj
+++ src/naptime/worker.clj
@@ -2,17 +2,22 @@
   (:require [clj-http.client :as http]
             [somnium.congomongo :as mon]))
 
-(defn fetch-and-lock-next-job! []
+(defn fetch-and-lock-next-job!
+  "Grabs the next job that's scheduled to be run. Atomically
+  locks job on fetch."
+  []
   (mon/fetch-and-modify
    :jobs
    {:locked false
-    :next-update {:$lt (System/currentTimeMillis)}}
+    :next-update {:$lte (System/currentTimeMillis)}}
    {:$set {:locked true}}
    :sort {:next-update -1}
    :upsert? false
    :return-new? true))
 
-(defn unlock-job! [job]
+(defn unlock-job!
+  "Unlocks a job."
+  [job]
   (mon/fetch-and-modify
    :jobs
    {:_id (:_id job)}
@@ -20,7 +25,9 @@
    :upsert? false
    :return-new? true))
 
-(defn with-next-job [f]
+(defn with-next-job
+  "Passes next job (possibly nil) to `f`. Handles locking / unlocking of the job."
+  [f]
   (let [job (fetch-and-lock-next-job!)]
     (try
       (f job)
@@ -35,9 +42,34 @@
                                              (:period job))}})
         (unlock-job! job))))))
 
+(defn log-job!
+  "Log interesting info about the job."
+  [wid endpoint period start-lag request-time]
+  (println wid
+           endpoint
+           period
+           request-time
+           start-lag)
+  (mon/insert! :job-logs {:worker-id wid
+                          :endpoint endpoint
+                          :period period
+                          :start-lag start-lag
+                          :request-time request-time
+                          :timestamp (System/currentTimeMillis)}))
+
+(defn log-worker!
+  "Log interesting information about the worker."
+  [wid used-capacity max-capacity]
+  #_(println wid used-capacity)
+  (mon/insert! :worker-logs {:worker-id wid
+                             :used-capacity used-capacity
+                             :max-capacity max-capacity
+                             :timestamp (System/currentTimeMillis)}))
+
 
 ;; refactor me!
-(defn run-loop! [used-capacity-atom max-capacity]
+(defn run-loop! [worker-id used-capacity-atom max-capacity]
+  (log-worker! worker-id @used-capacity-atom max-capacity)
   (if (< @used-capacity-atom max-capacity)
     (do
       (swap! used-capacity-atom inc)
@@ -46,11 +78,14 @@
           (future
             (try
               (when job
-                (println (:endpoint job)
-                         (:period job)
-                         (- (System/currentTimeMillis)
-                            (:next-update job)))
-                (http/get (:endpoint job)))
+                (let [start (System/currentTimeMillis)
+                      delta (- start (:next-update job))]
+                  (http/get (:endpoint job))
+                  (log-job! worker-id
+                            (:endpoint job)
+                            (:period job)
+                            delta
+                            (- (System/currentTimeMillis) start))))
               (finally
                (swap! used-capacity-atom dec)))))))))
 
