de7bc477a8a1fd391587874ffb12964bae1f66fa
zkim <zachary.kim@gmail.com>
2011-09-18 02:21:35 -0600

    Removed ning dependency.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "REST based URL hook scheduler."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [nsfw "0.2.4"]
-                 [com.ning/async-http-client "1.6.2"]
                  [congomongo "0.1.7-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [lein-marginalia "0.6.1"]]
