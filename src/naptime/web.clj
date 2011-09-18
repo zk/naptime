d54beecedc6be127f5f222d17ac01372254df33d
zkim <zachary.kim@gmail.com>
2011-09-18 00:59:01 -0600

    Added charting to web UI.

--- src/naptime/web.clj
+++ src/naptime/web.clj
@@ -46,20 +46,69 @@
   (html [:style {:type "text/css"}
          (apply str (map css-rule rules))]))
 
+(defn goog-sparkline [w h y-min y-max color data]
+  (str "https://chart.googleapis.com/chart?"
+       "chxt=y&"
+       "chds=" y-min "," y-max "&"
+       "chxr=0," y-min "," y-max "&"
+       "chs="
+       w
+       "x"
+       h
+       "&cht=lc&"
+       "chco=" color "&"
+       "chd=t:"
+       (->> data
+            (interpose ",")
+            (apply str))))
+
 
 ;;;;;;;;;;;;;;;
 ;; Templates ;;
 ;;;;;;;;;;;;;;;
 
-(def page-css
+(def about-page-css
+  (css
+   [:.about-job {:text-align "left"
+                 :width "600px"
+                 :margin-left "auto"
+                 :margin-right "auto"
+                 :position "relative"
+                 :margin-top "50px"}]
+   [".about-job .status" {:position "absolute"
+                          :top "0px"
+                          :right "0px"
+                          :color "green"
+                          :font-size "14pt"}]
+   [".about-job .status .label" {:color "#999"}]
+   [".about-job .warning" {:color "red"}]
+   [".about-job .period" {:position "absolute"
+                          :top "22px"
+                          :right "0px"
+                          :font-size "14pt"}]
+   [".about-job .period .label" {:color "#999"}]
+   [".about-job .charts" {:margin-top "60px"
+                          :margin-bottom "60px"}]
+   [".about-job .chart" {:text-align "center"}]
+   [".about-job .lag-chart" {:float "left"}]
+   [".about-job .request-chart" {:float "right"}]))
+
+(def main-css
   (css
    [:* {:padding :0px
         :margin :0px}]
+   [:.clear {:clear "both"}]
    [:h1 {:font-family "Helvetica"
          :margin-top "80px"
          :margin-bottom "30px"}]
    [:body {:font-family "Georgia"
            :text-align "center"}]
+   [:em {:font-weight "bold"
+         :font-style "normal"}]
+   [:a {:text-decoration "none"
+        :color "#88f"}]
+   [:a:hover {:text-decoration "none"
+              :color "blue"}]
    [:div.jobs {:width "600px"
                :margin-left "auto"
                :margin-right "auto"
@@ -83,15 +132,17 @@
                 :color "#777"}]
    [:td.about {:text-align "center"
                :width "115px"}]
-   [:a.about {:text-decoration "none"
-              :color "#88f"}]
-   [:a.about:hover {:text-decoration "none"
-                    :color "blue"}]
+   [:div.jobs-controls {:text-align "right"
+                        :width "640px"
+                        :margin-right "auto"
+                        :margin-left "auto"
+                        :padding-right "3px"}]
    [:div.create {:border "solid #ccc 1px"
                  :width "600px"
                  :margin-left "auto"
                  :margin-right "auto"
-                 :padding "20px"}]
+                 :padding "20px"
+                 :margin-bottom "30px"}]
    [:div.create:hover {:border "solid #999 1px"}]
    [:form {:padding "0px"
            :margin "0px"}]
@@ -100,7 +151,12 @@
    [:input.endpoint {:width "250px"
                      :margin-right "138px"}]
    [:input.period {:width "75px"
-                   :margin-right "10px"}]))
+                   :margin-right "10px"}]
+   [:.instructions {:color "#aaa"}]
+   [:.workers {:margin-bottom "30px"}]
+   [:.worker.chart {:margin-bottom "20px"}]))
+
+(def page-css (str main-css about-page-css))
 
 (defn render-jobs [jobs]
   (html
@@ -119,7 +175,11 @@
               [:td {:class "period"} period]
               [:td {:class "about"}
                [:a {:class "about"
-                    :href (str "/" (url-encode endpoint))} "about"]]])
+                    :href (str "/" (url-encode endpoint))} "about"]
+               "&nbsp;&nbsp;&nbsp;"
+               [:a {:class "delete"
+                    :href (str "/delete?endpoint=" (url-encode endpoint))}
+                "delete"]]])
           jobs)]]))
 
 (defn create-or-update-form []
@@ -137,26 +197,104 @@
               :name "create"
               :value "new / update"}]]]))
 
-(defn index [jobs]
+(defn render-jobs-controls []
+  (html
+   [:div {:class "jobs-controls"}
+    [:a {:href "/delete-all"} "clear all"]]))
+
+(defn main-tpl [& body]
   (html
    (doctype :html5)
    [:html
-    [:head page-css]
+    [:head
+     [:title "naptime! -- REST based URL hook scheduler."]
+     page-css]
     [:body
      [:h1 "Hooray, it's naptime!"]
-     (render-jobs jobs)
-     (create-or-update-form)]]))
+     body
+     [:div {:class "instructions"}
+      "refresh this page for the latest status info."]]]))
 
+(defn calc-load [worker]
+  (float (* (/ (:used-capacity worker)
+               (:max-capacity worker))
+            100)))
 
+(defn render-worker [worker]
+  (html
+   [:div {:class "chart worker"}
+    [:img {:src (goog-sparkline 500
+                                100
+                                0
+                                100
+                                "FF3333"
+                                (map calc-load (:logs worker)))
+           :width 500
+           :height 100}]
+    [:br]
+    "Load for worker "
+    [:em (:worker-id worker)]
+    " (%)"]))
+
+(defn render-workers [workers]
+  (html [:div {:class "workers"}
+         (map render-worker workers)]))
+
+(defn index [jobs workers]
+  (main-tpl (render-jobs-controls)
+            (render-jobs jobs)
+            (create-or-update-form)
+            (render-workers workers)))
+
+(defn about-tpl [{:keys [endpoint status period]}
+                 logs]
+  (let [start-lags (map :start-lag logs)
+        request-times (map :request-time logs)]
+    (main-tpl
+     [:div {:class "about-job"}
+      [:h2 endpoint]
+      [:span {:class "status"}
+       [:span {:class "label"} "last status: "]
+       [:span {:class (when-not (= "200" status) "warning")}
+        status]]
+      [:span {:class "period"}
+       [:span {:class "label"} "period (ms): "]
+       period]
+      [:div {:class "charts"}
+       [:div {:class "chart lag-chart"}
+        [:img {:src (goog-sparkline 275
+                                    50
+                                    (apply min start-lags)
+                                    (apply max start-lags)
+                                    "0077CC"
+                                    start-lags)
+               :width 275
+               :height 50}]
+        [:br]
+        "Start Time Lag (ms)"]
+       [:div {:class "chart request-chart"}
+        [:img {:src (goog-sparkline 275
+                                    50
+                                    (apply min request-times)
+                                    (apply max request-times)
+                                    "FF3333"
+                                    request-times)
+               :width 275
+               :height 50}
+         [:br]
+         "Request Time (ms)"]]
+       [:div {:class "clear"}]]])))
 
 ;;;;;;;;;;;;;;;
 ;; Responses ;;
 ;;;;;;;;;;;;;;;
 
+
+
 (defn error-response [endpoint period]
-  (-> (response (str "Sorry! Bad arguments -- {:endpoint " endpoint " :period " period "}"))
-      (status 422)
-      (header "Content-Type" "text/html")))
+    (-> (response (str "Sorry! Bad arguments -- {:endpoint " endpoint " :period " period "}"))
+        (status 422)
+        (header "Content-Type" "text/html")))
 
 (defn read [{:keys [endpoint]}]
   (render :text (mon/fetch-one :jobs :where {:endpoint endpoint})))
@@ -164,20 +302,26 @@
 (defn create [{:keys [endpoint period]}]
   (if (and endpoint period)
     (do (schedule! endpoint period)
-        (-> (response (format "Ok! Scheduled to hit %s every %f seconds." endpoint (double (/ period 1000))))
-            (status 200)))
+        (redirect "/"))
     (error-response endpoint period)))
 
 (defn delete [{:keys [endpoint period]}]
   (if endpoint
     (do (unschedule! endpoint)
-        (response (format "Ok! endpoint %s is unscheduled!"
-                          endpoint)))
-    (error-response endpoint period)))
+        (redirect "/"))))
 
 (defn about [endpoint]
   (fn [req]
-    (render :text (str (mon/fetch-one :jobs :where {:endpoint endpoint})))))
+    (render about-tpl
+            (mon/fetch-one :jobs :where {:endpoint endpoint})
+            (mon/fetch :job-logs
+                       :where {:endpoint endpoint}
+                       :sort {:timestamp -1}
+                       :limit 100))))
+
+(defn delete-all [req]
+  (mon/drop-coll! :jobs)
+  (redirect "/"))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -196,6 +340,18 @@
        :endpoint (-> req :params :endpoint)
        :period (parse-period (-> req :params :period))))))
 
+(defn worker-info []
+  (let [worker-ids (mon/distinct-values :worker-logs "worker-id"
+                                        :where {:timestamp {:$gt (- (System/currentTimeMillis)
+                                                                    1000)}})]
+    (->> worker-ids
+         (map #(hash-map :worker-id %
+                         :logs (mon/fetch :worker-logs
+                                          :where {:worker-id %}
+                                          :sort {:timestamp -1}
+                                          :limit 100)))
+         (sort-by :worker-id))))
+
 (def routes
   (mous/app
    wrap-stacktrace
@@ -203,13 +359,12 @@
    wrap-keyword-params
    wrap-args
    [""] (fn [r]
-          (-> (response (index (mon/fetch :jobs)))
+          (-> (response (index (mon/fetch :jobs)
+                               (worker-info)))
               (content-type "text/html")
               (status 200)))
-   ["read"] read
    ["create"] create
    ["delete"] delete
+   ["delete-all"] delete-all
    [endpoint] (about endpoint)))
-
-;;;
 
